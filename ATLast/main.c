8af16f1dfa939e4ab7e10d099d9251c2e5ed92b5
Michael Henderson <quoha@quoha.org>
2014-06-18 21:59:59 -0500

    Initial Commit

---
+++ ATLast/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ATLast
+//
+//  Created by Michael Henderson on 6/18/14.
+//  Copyright (c) 2014 Michael D Henderson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
